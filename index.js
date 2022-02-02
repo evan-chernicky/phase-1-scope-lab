cd7ad5522ea936aedeab02a506837d6d9a8a4c58
Evan Chernicky <echernicky@gmail.com>
2022-02-02 13:17:39 -0500

    commit tree 3267950283fe5e087f3bf9718c29c6f3fa14b7e0

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+//Uppercases Variable
+function upperCaseCustomerName() {
+    
+    customerName = customerName.toUpperCase();
+
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'BOB!';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'bob.';
+}
